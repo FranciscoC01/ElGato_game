7e2bb3dc2f88466218194b394220bfdd7a829c4d
FranciscoC01 <cfrancisco23273@gmail.com>
2022-10-14 00:33:28 -0300

    menu terminado al 95%, se comento el menu

--- main.c
+++ main.c
@@ -7,77 +7,75 @@
 void clear();
 void SetColor(int color);
 void clearArrayChar(char* array, int max_x, int val);
-int menu();
+void menu();
 
 int main(){
     menu();
     return 0;
 }
 
-int menu(){
-    int opt=0;
-    int flagPrint=0;
-    int flag=1,flagKeyPressed=1;
-    char in;
-    char c[]={62,32,32,32};
-    while(flag){
-        if(kbhit()){
-            if(flagKeyPressed){
-                in = getch();
-                printf("\n%d\n",in);
-                if(in=='w'){
-                    if(opt+1>3) opt=3;
-                    else opt++;
-                }
-                if(in=='s'){
-                    if(opt-1<0)opt=0;
-                    else opt--;
-                }
-                if((int)in==13){
-                    switch(opt){
-                        case 3:
-                            //funcion Nuevo Juego
-                            break;
-                        case 2:
-                            //funcion Continuar juego
-                            break;
-                        case 1:
-                            //funcion tabla puntuaciones
-                            break;
-                        case 0:
-                            flag=0;
-                            break;
-                        default:
-                            printf("\n\nError en el programa...\n\n");
-                    }
-                }
+//funcion menu - control principal de programa
+void menu(){
+    //variables
+    int opt=0;                                   //opcion elegida
+    int flagPrint=0;                             //bandera de impresion para evitar multiples impresiones
+    int flag=1;                                  //flag de bucle - si es 0 se sale del juego
+    char in;                                     //variable input del usuario
+    char c[]={62,32,32,32};                      //array para la parte grafica del menu
+
+    //programa
+    while(flag){                                 //bucle principal - mantiene vivo el menu
+        if(kbhit()){                             //si una tecla se presiono entonces se obtiene cual fue
+            in = getch();
+            if(in=='w'){                         //si es w entonces se sube el "puntero" y opt se le suma 1                      
+                if(opt+1>3) opt=3;
+                else opt++;
+            }
+            if(in=='s'){                         //si es s entonces se baja el "puntero" y opt se le resta 1
+                if(opt-1<0)opt=0;
+                else opt--;
+            }
+            if((int)in==13){                     //si es enter entonces se ingresa a esa opcion
                 switch(opt){
                     case 3:
-                        clearArrayChar(&c,4,32);
-                        c[0]=62;
+                        //funcion Nuevo Juego
                         break;
                     case 2:
-                        clearArrayChar(&c,4,32);
-                        c[1]=62;
+                        //funcion Continuar juego
                         break;
                     case 1:
-                        clearArrayChar(&c,4,32);
-                        c[2]=62;
+                        //funcion tabla puntuaciones
                         break;
                     case 0:
-                        clearArrayChar(&c,4,32);
-                        c[3]=62;
+                        flag=0;
                         break;
                     default:
                         printf("\n\nError en el programa...\n\n");
                 }
-                flagPrint=0;
-                flagKeyPressed=0;
             }
+            switch(opt){                       //switch que se encarga de mostrar graficamente que opcion se esta eligiendo
+                case 3:
+                    clearArrayChar(&c[0],4,32);
+                    c[0]=62;
+                    break;
+                case 2:
+                    clearArrayChar(&c[0],4,32);
+                    c[1]=62;
+                    break;
+                case 1:
+                    clearArrayChar(&c[0],4,32);
+                    c[2]=62;
+                    break;
+                case 0:
+                    clearArrayChar(&c[0],4,32);
+                    c[3]=62;
+                    break;
+                default:
+                    printf("\n\nError en el programa...\n\n");
+            }
+            flagPrint=0;
         }
-        else{
-            flagKeyPressed=1;
-        }
+        
         if(flagPrint == 0){
             clear();
             printf("===== EL GATO MATEMATICO =====\n");
@@ -85,13 +83,14 @@ int menu(){
             printf("%c2. Continuar partida\n",c[1]);
             printf("%c3. Estadisticas de partidas anteriores\n",c[2]);
             printf("%c4. salir\n",c[3]);
-            if(!flag) printf("Saliendo del juego...");
+            if(!flag) printf("\nSaliendo del juego...");
             flagPrint= 1; 
         }
     }
-    return 1;
+    return ;
 }
 
+//funcion que inicializa un array de tipo char a un valor especifico
 void clearArrayChar(char* array, int max_x, int val){
     int i;
     for(i=0;i<max_x;i++){
@@ -101,11 +100,14 @@ void clearArrayChar(char* array, int max_x, int val){
     return ;
 }
 
+//funcion que se encarga de limpiar la pantalla
 void clear(){
     system("cls");
     return ;
 }
 
+//funcion que cambia el color del texto
+//autor: desconocido - codigo compartido por el classroom
 void SetColor(int color) {
     WORD wColor; //This handle is needed to get the current background attribute
 
